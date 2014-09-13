01f8bb9d533c7354dc81067aee6d5ea83ea522a3
John HB <jhbco@hotmail.com>
2014-09-13 17:22:25 -0500

    sytling changes

--- js/myApp.js
+++ js/myApp.js
@@ -3,12 +3,42 @@ var myApp = angular.module('myApp',['ngRoute']);
 myApp.config(function($routeProvider){
 	//set up Routes
 	$routeProvider
+	.when('/index', {
+		templateUrl: 'index.html',
+		controller: 'indexController'
+	})
+	.when('/about', {
+		templateUrl: 'about.html',
+		controller: 'aboutController'
+	})
+	.when('/features', {
+		templateUrl: 'features.html',
+		controller: 'featuresController'
+	})
 	.when('/contactUs',{
 		templateUrl: 'contactUs.html',
 		controller: 'contactUsController'
+	})
+	.when('/', {
+		templateUrl: 'index.html',
+		controller: 'indexController'
 	});
 });
 
-myApp.controller('contactUsController', function($scope){
-	$scope.message = "Contact Us"
+myApp.controller('indexController', function($scope){
+	$scope.message = "Home";
 });
+
+myApp.controller('aboutController',function($scope){
+	$scope.message = "About";
+});
+
+myApp.controller('featuresController', function($scope){
+	$scope.message = "Features";
+});
+
+myApp.controller('contactUsController', function($scope){
+	$scope.message = "Contact Us";
+});
+
+
